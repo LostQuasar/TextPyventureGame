744a7c36beea415a8ea4f023ec423b4986934d9a
Spaceman <rionlion100@gmail.com>
2020-05-17 14:02:13 -0400

    Add initial movement parsing

--- zorkvisual.py
+++ zorkvisual.py
@@ -1,4 +1,4 @@
-import os, random, re, json
+import os, random, re, json, sys
 floors=[]
 floordata=[]
 cleanedfloor=[]
@@ -6,23 +6,45 @@ nicefloors=[]
 coll=[]
 location=[0x7F,0x7F,0x7F]
 health=100
-movementdir={'north':1,'east':2,'south':3,'west':4,'up':5,'down':6}
-
+errcodes={1:"Direction not found",2:"Something is blocking the way"}
 def moveplayer():
-    print("moved "+str(movementdir[userin.split(' ')[1].lower()]))
+    try:
+        errorcode=0
+        global location
+        if len(commandinput)>2:
+            movementdir=commandinput[1]+"_"+commandinput[2]
+        else:
+            movementdir=commandinput[1]
+        movementvalue=openroom(location,"direction")[movementdir]
+        if movementvalue==True:
+            print("Moving "+movementdir.replace("_"," "))
+        elif movementvalue==False:
+            errorcode=2
+        else:
+            print(movementvalue)
+    except KeyError:
+        errorcode=1        
+    return errorcode
 
-def openroom(loc):
+def openroom(loc, var):
     roomfile=''
     for num in range(0,3):
-        roomfile=roomfile+str(hex(loc[num])).replace("0x","")
+        roomfile=roomfile+str(hex(loc[num])[2:])
     with open(roomfile+'.json') as f:
-        data = json.load(f)
-        print(data['name'])
-
-commands={'move':moveplayer}
+        roomdata = json.load(f)
+        return roomdata[var]
+def quitgame():
+    sys.exit("Quiting...")
+commands={'move':moveplayer,'go':moveplayer,"quit":quitgame,"exit":quitgame}
 
 if __name__ == '__main__':
-    openroom(location)
-    userin=input('>>> ')
-    commandaction = commands[userin.split(' ')[0].lower()]
-    commandaction()
+    print(openroom(location, "name"))
+    while True:
+        try:
+            commandinput=input('>>> ').casefold().split(' ')
+        except KeyboardInterrupt:
+           quitgame()
+        commandaction=commands[commandinput[0].lower()]
+        error=commandaction()
+        if error!=0:
+            print(errcodes[error])
